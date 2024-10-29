0eb71c2e3b939aa387d6093d0783f9d1526a49e3
Michael Schwab <shorty@koeln.ccc.de>
2024-10-29 11:57:05 -0400

    Update and rename my-wallet.js to configs.js

--- configs.js
+++ configs.js
@@ -1,27 +1,5 @@
 /*
- *
- *          IOTA Command Line Wallet
- *
- * (c) by Michael Schwab <michael.schwab@mikeshouse.de>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 3 as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
- * For more information on the GPL, please go to:
- * http://www.gnu.org/copyleft/gpl.html
- *
- * For commercial purposes different licenses are
- * available from the author.
+
 */
 
 /*
